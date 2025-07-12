44a9f194753f7091a5a7b1ef7ecaa8fae2531198
petr <petr@petr-rome2-epyc>
2025-07-12 20:49:31 +0200

    fix-fe-be

--- portal-backend/src/main/java/com/kalafatic/web/UserBean.java
+++ portal-backend/src/main/java/com/kalafatic/web/UserBean.java
@@ -1,6 +1,9 @@
 package com.kalafatic.web;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserBean {
 	
--- portal-backend/src/main/java/com/kalafatic/web/UserServlet.java
+++ portal-backend/src/main/java/com/kalafatic/web/UserServlet.java
@@ -1,17 +1,26 @@
 package com.kalafatic.web;
 
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-import java.io.IOException;
-import java.sql.*;
-import java.util.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/users")
 public class UserServlet extends HttpServlet {
 
-    @Override
+    private static final long serialVersionUID = 1L;
+
+	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, IOException {
 
