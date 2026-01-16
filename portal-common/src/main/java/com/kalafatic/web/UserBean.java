2858c9fd568dff72b8b05b8e3cbb286ef3462371
petr <petr@petr-rome2-epyc>
2026-01-16 19:43:52 +0100

    version+

---
+++ portal-common/src/main/java/com/kalafatic/web/UserBean.java
@@ -0,0 +1,32 @@
+package com.kalafatic.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class UserBean {
+
+	public static boolean validate(String username, String password) {
+		try (Connection conn = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/portal?user=petr&password=traged")) {
+			PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE username=? AND password=?");
+			ps.setString(1, username);
+			ps.setString(2, password);
+
+			ResultSet rs = ps.executeQuery();
+			return rs.next();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public String getString(String key, String lang) {
+		Locale locale = new Locale(lang);
+		ResourceBundle bundle = ResourceBundle.getBundle("com.kalafatic.web.bundles.messages", locale);
+		return bundle.getString(key);
+	}
+}
